a399002ba323f9b9366841774f93e1863aae0a24
Samuel Spink <dr.greenjet@gmail.com>
2024-03-20 11:08:14 +0100

    feat: time checks

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from reader import read_data
 import time
 
 def main():
-    data = read_data()
+    data = read_data(file="SortMedium.txt")
     
     # Quicksort
     print("-- Quicksort --")    
@@ -14,7 +14,7 @@ def main():
     # Bubblesort
     print("-- Bubblesort --")
     start = time.time()
-    Bubblesort(data, 1)
+    Bubblesort(data, 2)
     end = time.time()
     bubble_time = end - start
     print(f"{bubble_time:.9f} Sekunden")
