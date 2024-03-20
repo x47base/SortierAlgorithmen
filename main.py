bc22db3a034923f58c5f7201ce363ee803837fa3
Samuel Spink <dr.greenjet@gmail.com>
2024-03-20 11:16:06 +0100

    feat: Quicksort time testing

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ import time
 
 def main():
     data = read_data(file="SortMedium.txt")
+    # Options:     (1) Nachname    (2) PLZ    (3) Geburtsdatum (dd.mm.yyyy)    (4) Vermögen (Decimal)
+    option = 1
     
     # Quicksort
     print("-- Quicksort --")    
+    start = time.time()
+    Quicksort(data, option)
+    end = time.time()
+    quicksort_time = end - start
+    print(f"{quicksort_time:.9f} Sekunden")
     
     # Bubblesort
     print("-- Bubblesort --")
     start = time.time()
-    Bubblesort(data, 2)
+    Bubblesort(data, option)
     end = time.time()
     bubble_time = end - start
     print(f"{bubble_time:.9f} Sekunden")
