3a93b50196a3f84911459f40ba12a3042d518df5
Samuel Spink <dr.greenjet@gmail.com>
2024-03-20 10:52:41 +0100

    feat: Time testing for bubble sorting

--- main.py
+++ main.py
@@ -3,10 +3,28 @@ from algorithms.bubblesort import Bubblesort
 from algorithms.heapsort import Heapsort
 from reader import read_data
 
+import time
+
 def main():
     data = read_data()
+    
+    # Quicksort
+    print("-- Quicksort --")    
+    
+    # Bubblesort
+    print("-- Bubblesort --")
+    start = time.time()
     Bubblesort(data, 1)
-    print(data)
+    end = time.time()
+    bubble_time = end - start
+    print(f"{bubble_time:.9f} Sekunden")
+    
+    # Heapsort
+    print("-- Heapsort --")
+    
+    # Another Sort Algorithm
+    print("-- Another Sort Algorithm --")
+    
 
 if __name__ == "__main__":
     main()
