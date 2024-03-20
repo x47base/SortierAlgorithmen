db1af3d5a59a0075519d857ad2fab4dc0661f1f4
leont <tschannenl@bzz.ch>
2024-03-20 11:06:07 +0100

    hu

--- algorithms/quicksort.py
+++ algorithms/quicksort.py
@@ -1,4 +1,24 @@
 
-def Quicksort():
-    pass
+def Quicksort(liste):
+    def partition(arr, low, high):
+        pivot = arr[high][2]
+        i = low - 1
+        for j in range(low, high):
+            if arr[j][2] <= pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        return i + 1
 
+    def quicksort_for_second_entry(arr, low, high):
+        if low < high:
+            pi = partition(arr, low, high)
+            quicksort_for_second_entry(arr, low, pi - 1)
+            quicksort_for_second_entry(arr, pi + 1, high)
+
+
+    quicksort_for_second_entry(liste, 0, len(liste) - 1)
+
+    # Ausgabe der sortierten Liste
+    for zeile in liste:
+        print(zeile)
