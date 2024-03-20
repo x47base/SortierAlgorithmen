8d506338ca5b5e1b16994aff6f6e5284eac366a2
Samuel Spink <dr.greenjet@gmail.com>
2024-03-20 11:39:26 +0100

    feat: Heapsort algorithm has been created

--- algorithms/heapsort.py
+++ algorithms/heapsort.py
@@ -1,3 +1,37 @@
+import csv
+import datetime
+
+
+def read_data(file: str = "SortSmall.txt", seperator: str = ","):
+    """
+
+    Read Data froma a CSV File
+    """
+    rows = []
+    with open(f"./data/{file}", "r") as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            to_int = [0, 4]
+            to_date = [5]
+            to_float = [6]
+
+            for i in to_int:
+                row[i] = int(row[i])
+
+            for i in to_date:
+                day_month_year = row[i].split(".")
+                row[i] = datetime.date(
+                    day=int(day_month_year[0]),
+                    month=int(day_month_year[1]),
+                    year=int(day_month_year[2]),
+                )
+
+            for i in to_float:
+                row[i] = float(row[i])
+
+            rows.append(row)
+        file.close()
+    return rows
 
 def Heapsort(array: list, sort_option: int):
     """
@@ -13,8 +47,36 @@ def Heapsort(array: list, sort_option: int):
     option = options[sort_option - 1]
 
     n = len(array)
-
-    def heapify():
-        pass
     
-    pass
+    def swap(x: int, y: int):
+        _x, _y = array[x], array[y]
+        array[y] = _x
+        array[x] = _y
+
+    def heapify(arr: list, i: int, heap_size: int):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        largest = i
+
+        if left < heap_size and arr[left][option] > arr[largest][option]:
+            largest = left
+        if right < heap_size and arr[right][option] > arr[largest][option]:
+            largest = right
+
+        if largest != i:
+            swap(i, largest)
+            heapify(arr, largest, heap_size)
+
+    def build_max_heap(arr: list):
+        for i in range(n // 2 - 1, -1, -1):
+            heapify(arr, i, n)
+
+    build_max_heap(array)
+    for i in range(n - 1, 0, -1):
+        swap(0, i)
+        heapify(array, 0, i)
+
+if __name__ == "__main__":
+    data = read_data()
+    Heapsort(data, 2)
+    print(data)
